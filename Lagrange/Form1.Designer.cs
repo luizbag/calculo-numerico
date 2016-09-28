c926137a310ee78daa7cff4a305433bf9dbf8f0d
Luiz Gavioli <luizbag@gmail.com>
2016-09-28 16:24:28 -0300

    Montando a curva

---
+++ Lagrange/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Lagrange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pcbImagem = new System.Windows.Forms.PictureBox();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chkOnline = new System.Windows.Forms.CheckBox();
+            this.lblPontos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbImagem
+            // 
+            this.pcbImagem.Image = ((System.Drawing.Image)(resources.GetObject("pcbImagem.Image")));
+            this.pcbImagem.InitialImage = ((System.Drawing.Image)(resources.GetObject("pcbImagem.InitialImage")));
+            this.pcbImagem.Location = new System.Drawing.Point(12, 12);
+            this.pcbImagem.Name = "pcbImagem";
+            this.pcbImagem.Size = new System.Drawing.Size(400, 400);
+            this.pcbImagem.TabIndex = 0;
+            this.pcbImagem.TabStop = false;
+            this.pcbImagem.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pcbImagem_MouseClick);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeColumns = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.CausesValidation = false;
+            this.grid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y});
+            this.grid.Location = new System.Drawing.Point(418, 28);
+            this.grid.MultiSelect = false;
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.grid.ShowCellErrors = false;
+            this.grid.ShowCellToolTips = false;
+            this.grid.ShowEditingIcon = false;
+            this.grid.ShowRowErrors = false;
+            this.grid.Size = new System.Drawing.Size(143, 384);
+            this.grid.TabIndex = 1;
+            this.grid.TabStop = false;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.ReadOnly = true;
+            this.X.Width = 50;
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            this.Y.ReadOnly = true;
+            this.Y.Width = 50;
+            // 
+            // chkOnline
+            // 
+            this.chkOnline.AutoSize = true;
+            this.chkOnline.Location = new System.Drawing.Point(418, 12);
+            this.chkOnline.Name = "chkOnline";
+            this.chkOnline.Size = new System.Drawing.Size(56, 17);
+            this.chkOnline.TabIndex = 2;
+            this.chkOnline.Text = "Online";
+            this.chkOnline.UseVisualStyleBackColor = true;
+            // 
+            // lblPontos
+            // 
+            this.lblPontos.AutoSize = true;
+            this.lblPontos.Location = new System.Drawing.Point(513, 13);
+            this.lblPontos.Name = "lblPontos";
+            this.lblPontos.Size = new System.Drawing.Size(48, 13);
+            this.lblPontos.TabIndex = 3;
+            this.lblPontos.Text = "0 pontos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 421);
+            this.Controls.Add(this.lblPontos);
+            this.Controls.Add(this.chkOnline);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.pcbImagem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Lagrange";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbImagem;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.CheckBox chkOnline;
+        private System.Windows.Forms.Label lblPontos;
+    }
+}
+
---
+++ Lagrange/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lagrange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap b;
+        private void pcbImagem_MouseClick(object sender, MouseEventArgs e)
+        {
+            b = (Bitmap)Bitmap.FromFile("Lagrange400.bmp");
+            b = (Bitmap)b.Clone();
+            if (e.Button == MouseButtons.Left)
+            {
+                if (!VerificarRepeticao(e.X - 200, -e.Y + 200))
+                {
+                    InserirDados(e.X - 200, -e.Y + 200);
+                    using (Graphics g = Graphics.FromImage(b))
+                    {
+                        for (int i = 0; i < grid.Rows.Count; i++)
+                        {
+                            int x = Convert.ToInt32(grid.Rows[i].Cells[0].Value);
+                            int y = Convert.ToInt32(grid.Rows[i].Cells[1].Value);
+
+                            g.FillEllipse(new SolidBrush(Color.Red), x + 198, -y + 198, 5, 5);
+                        }
+                    }
+                    pcbImagem.Image = b;
+                    pcbImagem.Refresh();
+                }
+            }
+            else
+            {
+                grid.Rows.Clear();
+                pcbImagem.Image = b;
+                pcbImagem.Refresh();
+            }
+        }
+
+        private void InserirDados(int x, int y)
+        {
+            grid.Rows.Add(new object[] { x, y });
+
+            lblPontos.Text = grid.Rows.Count + " pontos";
+            if (grid.Rows.Count > 1)
+            {
+                Interpolar();
+            }
+        }
+
+        private bool VerificarRepeticao(int x, int y)
+        {
+            int tam = grid.Rows.Count;
+            for (int i = 0; i < tam; i++)
+            {
+                double xi = Convert.ToInt32(grid.Rows[i].Cells[0].Value);
+                double yi = Convert.ToInt32(grid.Rows[i].Cells[1].Value);
+
+                if (x == xi && y == yi)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private double Lagrange(double x)
+        {
+            int tam = grid.Rows.Count;
+            double Li;
+            double y = 0;
+            y = 0;
+            for (int i = 0; i < tam; i++)
+            {
+                Li = 1;
+
+                double xi = Convert.ToInt32(grid.Rows[i].Cells[0].Value);
+                double yi = Convert.ToInt32(grid.Rows[i].Cells[1].Value);
+
+                for (int j = 0; j < tam; j++)
+                {
+                    int xj = Convert.ToInt32(grid.Rows[j].Cells[0].Value);
+
+                    if (i != j)
+                    {
+                        Li *= (x - xj) / (xi - xj);
+                    }
+                }
+
+                y += Li * yi;
+            }
+
+            return y;
+        }
+
+        private void Interpolar()
+        {
+            for (double x = -199; x < 200; x = x + 0.3)
+            {
+                double y0 = -Lagrange(x - 0.3) + 200;
+                double y1 = -Lagrange(x) + 200;
+
+                double x0 = (x - 0.3) + 200;
+                double x1 = x + 200;
+                if (!double.IsNaN(x1 + x0 + y0 + y1))
+                    using (Graphics g = Graphics.FromImage(b))
+                    {
+                        try
+                        {
+                            if (y0 < 0)
+                                y0 = -1;
+                            if (y0 > 400)
+                                y0 = 401;
+                            if (y1 > 400)
+                                y1 = 401;
+                            if (y1 < 0)
+                                y1 = -1;
+
+
+                            g.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+                        }
+                        catch { }
+                    }
+
+                if (chkOnline.Checked)
+                {
+                    pcbImagem.Image = b;
+                    pcbImagem.Refresh();
+                    Application.DoEvents();
+                }
+            }
+
+            if (!chkOnline.Checked)
+            {
+                pcbImagem.Image = b;
+                pcbImagem.Refresh();
+            }
+        }
+    }
+}
